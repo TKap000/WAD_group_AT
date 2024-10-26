29f2f2ea196a5903ebea056afb7c3f547c2f3b66
martinkaur <martinkaur96@gmail.com>
2024-10-26 20:36:23 +0300

    Half-way with json to html j-scripts.

--- src/js/script.js
+++ src/js/script.js
@@ -20,3 +20,99 @@ window.onclick = function(event) {
         }
     }
 }
+
+// TASK 3
+// READ HERE: https://jsonbin.io/api-reference
+let req = new XMLHttpRequest();
+var data;
+
+req.onreadystatechange = () => {
+    // if done recieving:
+  if (req.readyState == XMLHttpRequest.DONE) {
+    console.log(req.responseText);
+    data = req.data;
+    populatePosts();
+  }
+};
+
+req.open("GET", "https://api.jsonbin.io/v3/b/671d1b05e41b4d34e44932b2/latest", true);
+req.setRequestHeader("X-Master-Key", "$2a$10$oZq1VTb9o0etGF.YjwqYK.jxJnRYiA0Q1pPvSBpRi3txVzVSTAKGe");
+req.send();
+
+// TASK 4
+// JSON data displayer from URI (uniform resource identifier)
+function populatePosts() {
+
+            
+    const dataDisplay = document.getElementById("jsonDataDisplay");
+    // HELP here: https://stackoverflow.com/questions/34907982/looping-through-json-data-to-generate-html
+    // loop by object in data
+    for(var post in data){
+        const postElement = document.createElement("article");
+        dataDisplay.className("post"); // for style
+
+        const postHeaderElement = document.createElement("div");
+
+        const userIconElement = document.createElement("a");
+        userIconElement.className("user-icon-post");
+
+        const dateElement = document.createElement("p");
+        dateElement.textContent = data.created;
+
+        const postImgElement = document.createElement("img");
+        postImgElement.setAttribute("src", data.img)
+
+        const postTextElement = document.createElement("p");
+        postTextElement.textContent = data.created;
+
+        // HELP?
+        const postLikeElement = document.createElement("button");
+        postLikeElement.className("like-btn");
+
+        postHeaderElement.appendChild(userIconElement);
+        postHeaderElement.appendChild(dateElement);
+
+        postElement.appendChild(postHeaderElement);
+        postElement.appendChild(postImgElement);
+        postElement.appendChild(postTextElement);
+        postElement.appendChild(postLikeElement);
+
+        dataDisplay.appendChild(postElement);
+
+    }
+        
+    
+
+    // Append the elements to the "dataDisplay" div
+     
+}
+
+
+// TASK 5
+// JSON data displayer from local source
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('../../res/json/postitused.json')
+        .then(response => response.json())
+        .then(data => {
+            
+            const dataDisplay = document.getElementById("jsonDataDisplay");
+            // HELP here: https://stackoverflow.com/questions/34907982/looping-through-json-data-to-generate-html
+            /*
+            // Create HTML elements to display the JSON data
+            const nameElement = document.createElement("p");
+            nameElement.textContent = "Name: " + data.name;
+
+            const ageElement = document.createElement("p");
+            ageElement.textContent = "Age: " + data.age;
+
+            const cityElement = document.createElement("p");
+            cityElement.textContent = "City: " + data.city;
+            
+
+            // Append the elements to the "dataDisplay" div
+            dataDisplay.appendChild(nameElement);
+            dataDisplay.appendChild(ageElement);
+            dataDisplay.appendChild(cityElement); */
+        })
+        .catch(error => console.error("Error fetching JSON data:", error));
+});
