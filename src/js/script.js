3472a25647eb2ec9d5b6fceb61b1f6518f6481a1
martinkaur <martinkaur96@gmail.com>
2024-10-26 18:57:43 +0300

    Fixed broken file path for script.

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,11 @@
 function toggleDropdown() {
     const dropdown = document.getElementById("userDropdown");
     if (dropdown.style.display === "none" || dropdown.style.display === "") {
+        for (i = 0; i < dropdown.length; i++) {
+            dropdowns[i].style.display = "block";
+        }
         dropdown.style.display = "block";
+        
     } else {
         dropdown.style.display = "none";
     }
