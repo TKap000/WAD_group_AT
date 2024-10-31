9326c68d783324ac25bef3e1d03d0ebbbb7908fe
Martin Kaur <martinkaur96@gmail.com>
2024-10-31 19:28:50 +0200

    Extra features - comments (experimental). Styling improvements.

--- src/js/script.js
+++ src/js/script.js
@@ -21,9 +21,10 @@ window.onclick = function(event) {
     }
 }
 
+/*
 // TASK 3. UNCOMMENT TO TEST
 // READ HERE: https://jsonbin.io/api-reference
-/*
+
 let req = new XMLHttpRequest();
 req.responseType = 'json';
 
@@ -87,6 +88,11 @@ function populatePosts(data) {
         const postLikeElement = document.createElement("button");
         postLikeElement.className = "like-btn";
         postLikeElement.textContent = "👍";
+        
+        
+        //const postLikeCountElement = document.createElement("p");
+        //postLikeCountElement.className = "like-count";
+        //postLikeCountElement.textContent = data[post].likes;
 
         dataDisplay.appendChild(postElement);
 
@@ -94,18 +100,53 @@ function populatePosts(data) {
         postElement.appendChild(postImgElement);
         postElement.appendChild(postTextElement);
         postElement.appendChild(postLikeElement);
+        
 
         postHeaderElement.appendChild(userIconElement);
         userIconElement.appendChild(IconImgElement);
 
         postHeaderElement.appendChild(dateElement);
 
+        for(var c in data[post].comments){
+            //IF comment user != every user: make new img
+
+            const commentHeaderElement = document.createElement("div");
+            commentHeaderElement.className = "comment-header";
+
+            const postCommentElement = document.createElement("div");
+            postCommentElement.className = "post-comment-container";
+
+            const commentTextElement = document.createElement("p");
+            commentTextElement.className = "post-comment";
+            commentTextElement.textContent = data[post].comments[c].content;
+
+            const commentAuthorElement = document.createElement("p");
+            commentAuthorElement.className = "comment-author";
+            commentAuthorElement.textContent = data[post].comments[c].author;
+
+            const dateElement = document.createElement("p");
+            dateElement.className = "post-date";
+            dateElement.textContent = data[post].comments[c].created;
+
+            const CommentIconImgElement = document.createElement("img");
+            CommentIconImgElement.className = "user-icon-post";
+            CommentIconImgElement.setAttribute("src", "./res/images/me.png");
+
+            commentHeaderElement.appendChild(CommentIconImgElement);
+            commentHeaderElement.appendChild(commentAuthorElement);
+            commentHeaderElement.appendChild(dateElement);
+
+            postCommentElement.appendChild(commentHeaderElement);
+            postCommentElement.appendChild(commentTextElement);
+
+            postElement.appendChild(postCommentElement);
+        }
     }
-     
-}*/
+} 
+    */
 
-
-// TASK 5. This ONLY works on deployed page, browser security doesn't allow this kind of access to loacal filesystem.
+// TASK 5. This ONLY works on deployed page or if .json is hosted in a server, 
+// browser security doesn't allow this kind of access to loacal filesystem.
 // this is working example (or would be, structurally)
 // JSON data displayer from local source
 document.addEventListener("DOMContentLoaded", async function () {
@@ -163,8 +204,43 @@ document.addEventListener("DOMContentLoaded", async function () {
             userIconElement.appendChild(IconImgElement);
 
             postHeaderElement.appendChild(dateElement);
-        }
 
+            for(var c in data[post].comments){
+                //IF comment user != every user: make new img
+                //aka we don't have a massive library of user icons.
+
+                const commentHeaderElement = document.createElement("div");
+                commentHeaderElement.className = "comment-header";
+
+                const postCommentElement = document.createElement("div");
+                postCommentElement.className = "post-comment-container";
+
+                const commentTextElement = document.createElement("p");
+                commentTextElement.className = "post-comment";
+                commentTextElement.textContent = data[post].comments[c].content;
+
+                const commentAuthorElement = document.createElement("p");
+                commentAuthorElement.className = "comment-author";
+                commentAuthorElement.textContent = data[post].comments[c].author;
+
+                const dateElement = document.createElement("p");
+                dateElement.className = "post-date";
+                dateElement.textContent = data[post].comments[c].created;
+
+                const CommentIconImgElement = document.createElement("img");
+                CommentIconImgElement.className = "user-icon-post";
+                CommentIconImgElement.setAttribute("src", "./res/images/me.png");
+
+                commentHeaderElement.appendChild(CommentIconImgElement);
+                commentHeaderElement.appendChild(commentAuthorElement);
+                commentHeaderElement.appendChild(dateElement);
+
+                postCommentElement.appendChild(commentHeaderElement);
+                postCommentElement.appendChild(commentTextElement);
+
+                postElement.appendChild(postCommentElement);
+            }
+        }
     })
     .catch(error => console.error("Error fetching JSON data:", error));
 });
