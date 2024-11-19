3fc565467a67bdc1e702a7b66355609ed56e0b36
Jaak Unt <jaak.unt@ut.ee>
2024-11-19 19:35:30 +0200

    page templates

--- homework3/src/router/index.js
+++ homework3/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MainPage from '../views/MainPage.vue';
 
 const routes = [
   {
@@ -14,8 +15,19 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+  },
+  {
+    path: '/main',
+    name: 'main',
+    component: MainPage, // Add Main Page route
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: () => import(/* webpackChunkName: "signup" */ '../views/SignupPage.vue'), // Add Signup Page route
+  },
+];
+
 
 const router = createRouter({
   history: createWebHashHistory(),
---
+++ homework3/src/views/SignupPage.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <h1>Signup Page</h1>
+      <form>
+        <label for="name">Name:</label>
+        <input type="text" id="name" v-model="name" placeholder="Enter your name" />
+  
+        <label for="email">Email:</label>
+        <input type="email" id="email" v-model="email" placeholder="Enter your email" />
+  
+        <button type="submit" @click.prevent="submitForm">Sign Up</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "SignupPage",
+    data() {
+      return {
+        name: "",
+        email: "",
+      };
+    },
+    methods: {
+      submitForm() {
+        alert(`Signed up with Name: ${this.name}, Email: ${this.email}`);
+      },
+    },
+  };
+  </script>
+  
