abf70d9b178448babac7ab14d097721cafa16af0
kaspar <kasparkoolis@gmail.com>
2024-11-23 11:27:26 +0200

    NEW Header

--- homework3/src/components/Header.vue
+++ homework3/src/components/Header.vue
@@ -1,9 +1,9 @@
 <template>
     <header>
-      <nav>
-        <ul>
-          <li><router-link to="/main">Main</router-link></li>
-          <li><router-link to="/signup">Signup</router-link></li>
+      <nav class="nav-bar">
+        <ul class="nav-list">
+          <li class="nav-item"><router-link to="/main" class="nav-link">Main</router-link></li>
+          <li class="nav-item"><router-link to="/signup" class="nav-link">Signup</router-link></li>
         </ul>
       </nav>
     </header>
@@ -17,5 +17,42 @@
   
   <style>
   /* styling needs to go here!  */
+  
+  header {
+    background-color: aliceblue;
+    padding: 10px 20px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    border-radius: 10px;
+  }
+  
+  .nav-bar {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .nav-list {
+    list-style: none;
+    display: flex;
+    margin: 0;
+    padding: 0;
+    gap: 20px;
+  }
+
+  .nav-item {
+    font-size: 18px;
+    font-weight: bold;
+  }  
+
+  .nav-link {
+    text-decoration: none;
+    color: black;
+    transform: color 0.3s;
+  }
+
+  .nav-link:hover {
+    color: #0fb80c;
+  }
+  
   </style>
   
